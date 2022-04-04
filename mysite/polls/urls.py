bfcbecc25c93f9c61ed1f47af6bd4277f468f294
apxtt <dfwtt11@gmail.com>
2022-04-04 11:30:29 +0900

    django document part5 start

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'), # /polls
     path('<int:question_id>/', views.detail, name='detail'), #질문 페이지 /polls/<question_id>/
